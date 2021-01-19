215c1fdd5ef22bc448ea92906f91e3b19e445dd9
mjones1818 <michael.jones0532@gmail.com>
2021-01-19 15:45:50 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(array, mapFunction) {
+  let results = []
+  for (let element of array) {
+    results.push(mapFunction(element))
+  }
+  return results
+}
+
+function reduce(array, reduceFunction, starting) {
+  let element = (!!starting) ? starting : array[0]
+  let i = (!!starting) ? 0 : 1
+  for (; i < array.length; i++) {
+    element = reduceFunction(array[i], element)
+  }
+  return element
+}
